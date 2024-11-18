7b004285178effab7b32a21dfb6794d59829d2ed
Melissa Montesinos <melissa.d.montesinos@gmail.com>
2024-11-17 20:23:07 -0500

    Set up App component with initial product list

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# React + Vite
+# Coding-Project-16
 
+ Product Dashboard Setup Using React and Vite
+ 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [products, setProducts] = useState([ // Defining a default list of products
+    {id: 1,name: "Dysol Airway Multi-Styler Complete-Long", price: 599.99, description: "Strawberry Bronze/Blusk Pink"},
+    {id: 2, name: "Ninja - CREAMi, Ice Cream Meker", price: 199.99, description: "7 One-Touch Programs - Cloud Silver"},
+    {id: 3, name: "Withings - ScanWatch Light", price: 199.95, description: "37 mm Sand & Rose Gold"},
+    {id: 4, name: "Apple - 13-inch iPad Air", price: 799.00, description: "Starlight"},
+  ]);
+
+  const addProduct = (newProduct) => {
+    setProducts([...product, {...newProduct}]);
+  };
 
   return (
     <>
-
+      <div>
+        <h1></h1>
+      </div>
     </>
   )
-}
+};
 
 export default App
